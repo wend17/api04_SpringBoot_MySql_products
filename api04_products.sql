47843dce0b212bb625d96287ffb912937a2fa4c0
Wendy <wendyvillazana@gmail.com>
2023-02-02 16:32:46 -0500

    añadiendo script

---
+++ api04_products.sql
@@ -0,0 +1,14 @@
+create database  api04_products;
+
+use api04_products;
+
+
+select * from products;
+insert into products (id, first_name) values  (1,'producto 1');
+insert into products (id, first_name) values  (2,'producto 2');
+insert into products (id, first_name) values  (3,'producto 3');
+insert into products (id, first_name) values  (4,'producto 4');
+insert into products (id, first_name) values  (5,'producto 5');
+insert into products (id, first_name) values  (6,'producto 6');
+insert into products (id, first_name) values  (7,'producto 7');
+insert into products (id, first_name) values  (8,'producto 8');
